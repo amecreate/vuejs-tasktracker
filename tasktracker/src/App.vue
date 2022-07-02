54f8106dbc227844983592e4751e4f355779bbab
Amy L <amelieasmile@gmail.com>
2022-07-02 16:32:19 -0400

    add v-if statement to div which checks the value in data, and toggles the visibility of div

--- tasktracker/src/App.vue
+++ tasktracker/src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="container">
   <Header title="Task Tracker"/>
+  <div v-if="showAddTask">
   <AddTask @add-task="addTask" />
+  </div>
   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
 </div>
 </template>
@@ -19,7 +21,8 @@ export default {
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false
     }
   },
   methods: {
