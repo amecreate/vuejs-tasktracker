06d3a3822d829729b4f98b30d606a265500863b6
Amy L <amelieasmile@gmail.com>
2022-07-03 10:08:01 -0400

    add fetchTasks method in App.vue to fetch data from the backend (json-server)

--- tasktracker/src/App.vue
+++ tasktracker/src/App.vue
@@ -40,6 +40,11 @@ export default {
     },
     toggleReminder(id) {
       this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    },
+    async fetchTasks() {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+      return data
     }
   },
   created() {
