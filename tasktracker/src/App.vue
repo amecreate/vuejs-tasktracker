d52daeb6de5a3fd556e337ed9720cd04540d82e0
Amy L <amelieasmile@gmail.com>
2022-07-03 10:14:21 -0400

    add fetchTask() method to get a single task by id

--- tasktracker/src/App.vue
+++ tasktracker/src/App.vue
@@ -45,7 +45,12 @@ export default {
       const res = await fetch('http://localhost:5000/tasks')
       const data = await res.json()
       return data
-    }
+    },
+  async fetchTask(id) {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
   },
   async created() {
     this.tasks = await this.fetchTasks()
